38fbe55ffcd88efa4e264f3233ffd4ca3f2df81f
mp <miguel@mconf.com>
2024-11-22 10:05:06 -0300

    Removidos comentários desnecessários

--- 2_2.py
+++ 2_2.py
@@ -26,12 +26,3 @@ translation_chain = LLMChain(llm=llm, prompt=prompt_template)
 async def translate_text(input: TextInput):
     translation = translation_chain.invoke(input.text)
     return {"input": input.text, "translation": translation}
-
-'''
-Set OPENAI_API_KEY in .env
-
-Executar com:
-curl -X POST "http://127.0.0.1:8000/translate/"
--H "Content-Type: application/json"
--d '{"text": "I am a Data Scientist and work with machine learning models on a daily basis."}'
-'''
